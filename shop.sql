8b345c0aa6d03d618a3f6e535685eb77ccbe7ec9
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2020-09-15 15:28:18 +0700

    add database information

--- shop.sql
+++ shop.sql
@@ -1,27 +1,6 @@
--- MySQL dump 10.13  Distrib 8.0.14, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: shop
--- ------------------------------------------------------
--- Server version	8.0.14
-
---!40101 SET @OLD_CHARACTER_SET_CLIENT= */;
---!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
---!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES 'utf8';
---!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
---!40103 SET TIME_ZONE='+00:00' */;
---!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
---!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
---!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
---!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET NAMES 'utf8';
 
 
---
--- Table structure for table "user"
---
-
---DROP TABLE IF EXISTS "user";
---!40101 SET @saved_cs_client     =  */;
 CREATE TABLE "user" (
   "id" uuid,
   "firstName" varchar(50) DEFAULT NULL,
@@ -38,36 +17,8 @@ CREATE TABLE "user" (
   "profile" text,
   PRIMARY KEY ("id")
 );
---!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table "user"
---
-
---BEGIN WORK;
---LOCK TABLE user IN SHARE ROW EXCLUSIVE MODE;
---!40000 ALTER TABLE "user" DISABLE KEYS */;
---!40000 ALTER TABLE "user" ENABLE KEYS */;
---COMMIT WORK;
---!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
---!40101 SET SQL_MODE=@OLD_SQL_MODE */;
---!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
---!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
---!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
---!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2020-04-27  6:46:35
 
 
---
--- Table structure for table "cart"
---
-
---DROP TABLE IF EXISTS "cart";
---!40101 SET @saved_cs_client     =  */;
 CREATE TABLE "cart" (
   "id" uuid,
   "userId" uuid DEFAULT NULL,
@@ -90,21 +41,8 @@ CREATE TABLE "cart" (
   PRIMARY KEY ("id"),
   CONSTRAINT "fk_cart_user" FOREIGN KEY ("userId") REFERENCES "user" ("id")
 );
---!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table "cart"
---
-
---BEGIN WORK;
---LOCK TABLE cart IN SHARE ROW EXCLUSIVE MODE;
---!40000 ALTER TABLE "cart" DISABLE KEYS */;
---!40000 ALTER TABLE "cart" ENABLE KEYS */;
---COMMIT WORK;
 
 
---DROP TABLE IF EXISTS "product";
---!40101 SET @saved_cs_client     =  */;
 CREATE TABLE "product" (
   "id" uuid,
   "userId" uuid NOT NULL,
@@ -127,25 +65,8 @@ CREATE TABLE "product" (
   PRIMARY KEY ("id"),
   CONSTRAINT "fk_product_user" FOREIGN KEY ("userId") REFERENCES "user" ("id")
 );
---!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table "product"
---
-
---BEGIN WORK;
---LOCK TABLE product IN SHARE ROW EXCLUSIVE MODE;
---!40000 ALTER TABLE "product" DISABLE KEYS */;
---!40000 ALTER TABLE "product" ENABLE KEYS */;
---COMMIT WORK;
 
 
---
--- Table structure for table "cart_item"
---
-
---DROP TABLE IF EXISTS "cart_item";
---!40101 SET @saved_cs_client     =  */;
 CREATE TABLE "cart_item" (
   "id" uuid,
   "productId" uuid NOT NULL,
@@ -162,24 +83,8 @@ CREATE TABLE "cart_item" (
   CONSTRAINT "fk_cart_item_cart" FOREIGN KEY ("cartId") REFERENCES "cart" ("id"),
   CONSTRAINT "fk_cart_item_product" FOREIGN KEY ("productId") REFERENCES "product" ("id")
 );
---!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table "cart_item"
---
 
---BEGIN WORK;
---LOCK TABLE cart_item IN SHARE ROW EXCLUSIVE MODE;
---!40000 ALTER TABLE "cart_item" DISABLE KEYS */;
---!40000 ALTER TABLE "cart_item" ENABLE KEYS */;
---COMMIT WORK;
-
---
--- Table structure for table "category"
---
-
---DROP TABLE IF EXISTS "category";
---!40101 SET @saved_cs_client     =  */;
 CREATE TABLE "category" (
   "id" uuid,
   "parentId" uuid DEFAULT NULL,
@@ -190,24 +95,8 @@ CREATE TABLE "category" (
   PRIMARY KEY ("id"),
   CONSTRAINT "fk_category_parent" FOREIGN KEY ("parentId") REFERENCES "category" ("id")
 );
---!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table "category"
---
 
---BEGIN WORK;
---LOCK TABLE category IN SHARE ROW EXCLUSIVE MODE;
---!40000 ALTER TABLE "category" DISABLE KEYS */;
---!40000 ALTER TABLE "category" ENABLE KEYS */;
---COMMIT WORK;
-
---
--- Table structure for table "order"
---
-
---DROP TABLE IF EXISTS "order";
---!40101 SET @saved_cs_client     =  */;
 CREATE TABLE "order" (
   "id" uuid,
   "userId" uuid DEFAULT NULL,
@@ -238,24 +127,8 @@ CREATE TABLE "order" (
   PRIMARY KEY ("id"),
   CONSTRAINT "fk_order_user" FOREIGN KEY ("userId") REFERENCES "user" ("id")
 );
---!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table "order"
---
 
---BEGIN WORK;
---LOCK TABLE order IN SHARE ROW EXCLUSIVE MODE;
---!40000 ALTER TABLE "order" DISABLE KEYS */;
---!40000 ALTER TABLE "order" ENABLE KEYS */;
---COMMIT WORK;
-
---
--- Table structure for table "order_item"
---
-
---DROP TABLE IF EXISTS "order_item";
---!40101 SET @saved_cs_client     =  */;
 CREATE TABLE "order_item" (
   "id" uuid,
   "productId" uuid NOT NULL,
@@ -271,28 +144,8 @@ CREATE TABLE "order_item" (
   CONSTRAINT "fk_order_item_order" FOREIGN KEY ("orderId") REFERENCES "order" ("id"),
   CONSTRAINT "fk_order_item_product" FOREIGN KEY ("productId") REFERENCES "product" ("id")
 );
---!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table "order_item"
---
 
---BEGIN WORK;
---LOCK TABLE order_item IN SHARE ROW EXCLUSIVE MODE;
---!40000 ALTER TABLE "order_item" DISABLE KEYS */;
---!40000 ALTER TABLE "order_item" ENABLE KEYS */;
---COMMIT WORK;
-
---
--- Table structure for table "product"
---
-
---
--- Table structure for table "product_category"
---
-
---DROP TABLE IF EXISTS "product_category";
---!40101 SET @saved_cs_client     =  */;
 CREATE TABLE "product_category" (
   "productId" uuid NOT NULL,
   "categoryId" uuid NOT NULL,
@@ -300,24 +153,8 @@ CREATE TABLE "product_category" (
   CONSTRAINT "fk_pc_category" FOREIGN KEY ("categoryId") REFERENCES "category" ("id"),
   CONSTRAINT "fk_pc_product" FOREIGN KEY ("productId") REFERENCES "product" ("id")
 );
---!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table "product_category"
---
 
---BEGIN WORK;
---LOCK TABLE product_category IN SHARE ROW EXCLUSIVE MODE;
---!40000 ALTER TABLE "product_category" DISABLE KEYS */;
---!40000 ALTER TABLE "product_category" ENABLE KEYS */;
---COMMIT WORK;
-
---
--- Table structure for table "product_meta"
---
-
---DROP TABLE IF EXISTS "product_meta";
---!40101 SET @saved_cs_client     =  */;
 CREATE TABLE "product_meta" (
   "id" uuid,
   "productId" uuid NOT NULL,
@@ -326,24 +163,8 @@ CREATE TABLE "product_meta" (
   PRIMARY KEY ("id"),
   CONSTRAINT "fk_meta_product" FOREIGN KEY ("productId") REFERENCES "product" ("id")
 );
---!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table "product_meta"
---
 
---BEGIN WORK;
---LOCK TABLE product_meta IN SHARE ROW EXCLUSIVE MODE;
---!40000 ALTER TABLE "product_meta" DISABLE KEYS */;
---!40000 ALTER TABLE "product_meta" ENABLE KEYS */;
---COMMIT WORK;
-
---
--- Table structure for table "product_review"
---
-
---DROP TABLE IF EXISTS "product_review";
---!40101 SET @saved_cs_client     =  */;
 CREATE TABLE "product_review" (
   "id" uuid,
   "productId" uuid NOT NULL,
@@ -358,21 +179,8 @@ CREATE TABLE "product_review" (
   CONSTRAINT "fk_review_parent" FOREIGN KEY ("parentId") REFERENCES "product_review" ("id"),
   CONSTRAINT "fk_review_product" FOREIGN KEY ("productId") REFERENCES "product" ("id")
 );
---!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table "product_review"
---
 
---BEGIN WORK;
---LOCK TABLE product_review IN SHARE ROW EXCLUSIVE MODE;
---!40000 ALTER TABLE "product_review" DISABLE KEYS */;
---!40000 ALTER TABLE "product_review" ENABLE KEYS */;
---COMMIT WORK;
-
---
--- Table structure for table "product_tag"
---
 
 CREATE TABLE "tag" (
   "id" uuid,
@@ -382,20 +190,8 @@ CREATE TABLE "tag" (
   "content" text,
   PRIMARY KEY ("id")
 );
---!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table "tag"
---
 
---BEGIN WORK;
---LOCK TABLE tag IN SHARE ROW EXCLUSIVE MODE;
---!40000 ALTER TABLE "tag" DISABLE KEYS */;
---!40000 ALTER TABLE "tag" ENABLE KEYS */;
---COMMIT WORK;
-
---DROP TABLE IF EXISTS "product_tag";
---!40101 SET @saved_cs_client     =  */;
 CREATE TABLE "product_tag" (
   "productId" uuid NOT NULL,
   "tagId" uuid NOT NULL,
@@ -403,31 +199,8 @@ CREATE TABLE "product_tag" (
   CONSTRAINT "fk_pt_product" FOREIGN KEY ("productId") REFERENCES "product" ("id"),
   CONSTRAINT "fk_pt_tag" FOREIGN KEY ("tagId") REFERENCES "tag" ("id")
 );
---!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table "product_tag"
---
 
---BEGIN WORK;
---LOCK TABLE product_tag IN SHARE ROW EXCLUSIVE MODE;
---!40000 ALTER TABLE "product_tag" DISABLE KEYS */;
---!40000 ALTER TABLE "product_tag" ENABLE KEYS */;
---COMMIT WORK;
-
---
--- Table structure for table "tag"
---
-
---DROP TABLE IF EXISTS "tag";
---!40101 SET @saved_cs_client     =  */;
-
---
--- Table structure for table "transaction"
---
-
---DROP TABLE IF EXISTS "transaction";
---!40101 SET @saved_cs_client     =  */;
 CREATE TABLE "transaction" (
   "id" uuid,
   "userId" uuid NOT NULL,
@@ -443,16 +216,5 @@ CREATE TABLE "transaction" (
   CONSTRAINT "fk_transaction_order" FOREIGN KEY ("orderId") REFERENCES "order" ("id"),
   CONSTRAINT "fk_transaction_user" FOREIGN KEY ("userId") REFERENCES "user" ("id")
 );
---!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table "transaction"
---
-
---BEGIN WORK;
---LOCK TABLE transaction IN SHARE ROW EXCLUSIVE MODE;
---!40000 ALTER TABLE "transaction" DISABLE KEYS */;
---!40000 ALTER TABLE "transaction" ENABLE KEYS */;
---COMMIT WORK;
 
 --from https://github.com/tutorials24x7/shopping-cart-database-mysql/blob/master/shop.sql
