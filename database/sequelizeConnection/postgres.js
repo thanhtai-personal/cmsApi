b35a746c79ac7986c32247f1a3a12f737954504f
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2020-09-18 20:22:08 +0700

    update database config

--- database/sequelizeConnection/postgres.js
+++ database/sequelizeConnection/postgres.js
@@ -1,4 +1,5 @@
 const { Sequelize } = require('sequelize');
+const config = require('./config.json');
 
 var DatabaseSingleton = (function(){
   var instance;
@@ -6,32 +7,30 @@ var DatabaseSingleton = (function(){
     getInstance: function(){
       // check if instance is available
       if (!instance) {
+        let databaseConfig;
         if (process.env.NODE_ENV === 'production') {
-          instance = new Sequelize('d970hatls108fr', 'xwlmrkzbcrnoxp', 'bd2ca6cf649338bb6c9f2cc482c1d5902785825bc1733682c61107515ce6e072', {
-            host: 'ec2-50-19-231-222.compute-1.amazonaws.com',
-            dialect: 'postgres',
-            port: 5432,
-          })
+          databaseConfig = config.production
         } else {
-          instance = new Sequelize('shop', 'postgres', '', {
-            host: 'localhost',
-            dialect: 'postgres',
-            port: 5432,
-        
-            logging: console.log,                  // Default, displays the first parameter of the log function call
-            // logging: (...msg) => console.log(msg), // Displays all log function call parameters
-            // logging: false,                        // Disables logging
-            // logging: msg => logger.debug(msg),     // Use custom logger (e.g. Winston or Bunyan), displays the first parameter
-            // logging: logger.debug.bind(logger)     // Alternative way to use custom logger, displays all messages
-        
-            // pool: {
-            //   max: 5,
-            //   min: 0,
-            //   acquire: 30000,
-            //   idle: 10000
-            // }
-          })
+          databaseConfig = config.development
         }
+        instance = new Sequelize(databaseConfig.databaseName, databaseConfig.userName, databaseConfig.password, {
+          host: databaseConfig.host,
+          dialect: 'postgres',
+          port: 5432,
+      
+          logging: console.log,                  // Default, displays the first parameter of the log function call
+          // logging: (...msg) => console.log(msg), // Displays all log function call parameters
+          // logging: false,                        // Disables logging
+          // logging: msg => logger.debug(msg),     // Use custom logger (e.g. Winston or Bunyan), displays the first parameter
+          // logging: logger.debug.bind(logger)     // Alternative way to use custom logger, displays all messages
+      
+          // pool: {
+          //   max: 5,
+          //   min: 0,
+          //   acquire: 30000,
+          //   idle: 10000
+          // }
+        })
         delete instance.constructor; // or set it to null
       }
       return instance;
